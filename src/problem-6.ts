a3a482e4c61bc53b96c28b79c7829c5a0fe5a5f4
nurhossainfarid <faahsan1516@gmail.com>
2024-11-07 22:43:19 +0600

    solve problem-7

---
+++ src/problem-6.ts
@@ -0,0 +1,20 @@
+{
+  /**
+       * Problem-6: Define an interface Profile with properties name, age, and email. Create a function 
+        updateProfile that accepts an object of type Profile and an object of type Partial representing 
+        the updates. The function should return the updated profile.
+      */
+
+    interface Profile {
+        name: string;
+        age: number;
+        email: string;
+    }
+
+    // create a updateProfile function
+    const updateProfile = (profile, data) => {
+
+    }
+
+  //
+}
---
+++ src/problem-7.ts
@@ -0,0 +1,29 @@
+{
+  /**
+       * Problem-7: Create a TypeScript class Car with properties make, model, and year. Include a 
+        method getCarAge that returns the car's age based on the current year.
+      */
+
+  class Car {
+    make: string;
+    model: string;
+    year: number;
+
+    constructor(make: string, model: string, year: number) {
+      this.make = make;
+      this.model = model;
+      this.year = year;
+    }
+
+    // getCarAge
+    getCarAge():string {
+      const currentYear: number = new Date().getFullYear();
+      const carAge: number = currentYear - this.year;
+      return `${carAge} (assuming current year is 2024)`;
+    }
+  }
+
+  // Sample input
+  const car = new Car("Honda", "Civic", 2018);
+  console.log(car.getCarAge());
+}
