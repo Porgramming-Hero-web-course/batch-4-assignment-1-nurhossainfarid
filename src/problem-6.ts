e7d9a1f6ae3ae9db469686c5382c890244c40fc7
nurhossainfarid <faahsan1516@gmail.com>
2024-11-08 14:18:23 +0600

    solve problem-6

--- src/problem-6.ts
+++ src/problem-6.ts
@@ -5,16 +5,28 @@
         the updates. The function should return the updated profile.
       */
 
-    interface Profile {
-        name: string;
-        age: number;
-        email: string;
-    }
+  // Profile Interface
+  interface Profile {
+    name: string;
+    age: number;
+    email: string;
+  }
 
-    // create a updateProfile function
-    const updateProfile = (profile, data) => {
+  // UpdateData type
+  type UpdateData<T> = {
+    [P in keyof T]?: T[P];
+  };
 
-    }
+  // create a updateProfile function
+  const updateProfile = (profile: Profile, data: UpdateData<Profile>): Profile => {
+    const updateData: Profile = { ...profile, ...data };
+    return updateData;
+  };
+
+  // Sample Input
+  const myProfile = { name: "Alice", age: 25, email: "alice@example.com" };
+  // Sample Output
+  console.log(updateProfile(myProfile, { age: 23, name: "Nur Hossain" }));// { name: 'Nur Hossain', age: 23, email: 'alice@example.com' }
 
   //
 }
